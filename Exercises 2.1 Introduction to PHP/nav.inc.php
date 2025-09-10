cd24678a8ee01943d1a5847c1951665d52758181
Gian Carlos <117975442+GianCarlos25@users.noreply.github.com>
2025-09-10 18:57:23 +0200

    Add files via upload

---
+++ Exercises 2.1 Introduction to PHP/nav.inc.php
@@ -0,0 +1,13 @@
+<?php
+echo "<nav class=\"navigation\">
+        <ul>
+            <li><a href=\"principal.php\">Ir a Principal</a></li>
+        </ul>
+        <ul>
+            <li><a href=\"tecnologias.php\">Ir a Tecnologías</a></li>
+        </ul>
+        <ul>
+            <li><a href=\"rrss.php\">Ir a Redes Sociales</a></li>
+        </ul>
+    </nav>";
+?>
---
+++ Exercises 2.1 Introduction to PHP/web/tecnologias.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Tecnologías</title>
+</head>
+
+<body>
+    <header>
+        <h1>Bienvenido a la Página de Tecnologías</h1>
+    </header>
+    <?php include '../nav.inc.php'; ?>
+    <section>
+        <article>
+            <h2>Tecnologías que utilizo</h2>
+            <p>En mi trabajo y proyectos personales, utilizo una variedad de tecnologías para el desarrollo web,
+                incluyendo:</p>
+            <ul>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>JavaScript </li>
+                <li>Frameworks como React y CakePHP</li>
+                <li>PHP y Python para el backend</li>
+                <li>Bases de datos como MyPHP y MySQL</li>
+            </ul>
+            <p>Estoy siempre abierto a aprender nuevas tecnologías y mejorar mis habilidades.</p>
+        </article>
+        <article class="back-to-principal">
+            <a href="principal.html">
+                <div>Volver al Inicio</div>
+            </a>
+        </article>
+    </section>
+
+</body>
+
+</html>
